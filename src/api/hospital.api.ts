6f0ee4884f51cc9ff954db50773daf5ea6b1e0fe
phj <aaagowns@gmail.com>
2024-06-08 18:56:09 +0900

    병원 api 구조 변경 함수 추가

--- src/api/hospital.api.ts
+++ src/api/hospital.api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
-import { Hospital } from "../pages/Result";
+import { FormatHospital, Hospital } from "../model/hospital.model";
 
-export const getHospital = async (yadmNm: string, clCd?: string, dgsbjtCd?: string) => {
+export const getHospitals = async (yadmNm: string, clCd?: string, dgsbjtCd?: string) => {
   const latitude = localStorage.getItem("Latitude");
   const longitude = localStorage.getItem("Longitude");
 
   let url = `${import.meta.env.VITE_SERVICE_URL}?serviceKey=${
     import.meta.env.VITE_SERVICE_KEY
-  }&yadmNm=${yadmNm}&pageNo=1&numOfRows=50&xPos=${longitude}&yPos=${latitude}&radius=20000`;
+  }&yadmNm=${yadmNm}&pageNo=1&numOfRows=10&xPos=${longitude}&yPos=${latitude}&radius=20000`;
 
   if (clCd) {
     url += `&clCd=${clCd}`;
@@ -19,5 +19,18 @@ export const getHospital = async (yadmNm: string, clCd?: string, dgsbjtCd?: stri
 
   const response: Hospital[] = (await axios.get(url)).data.response.body.items.item;
 
-  return response;
+  // 데이터 구조 변경
+  const formatData: FormatHospital[] = response.map((hospital) => {
+    return {
+      yadmNm: hospital.yadmNm,
+      addr: hospital.addr,
+      telno: hospital.telno,
+      XPos: hospital.XPos,
+      YPos: hospital.YPos,
+      drTotCnt: hospital.drTotCnt,
+      distance: Math.floor(parseInt(hospital.distance, 10) / 1000) + "km",
+    };
+  });
+
+  return formatData;
 };
